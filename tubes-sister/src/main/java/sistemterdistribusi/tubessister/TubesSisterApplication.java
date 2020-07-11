b019e3793709cc6780e5dca1bdc20b6366cc8dbd
AfifRana <36437450+AfifRana@users.noreply.github.com>
2020-07-11 17:09:02 +0700

    init spring web

---
+++ tubes-sister/src/main/java/sistemterdistribusi/tubessister/TubesSisterApplication.java
@@ -0,0 +1,13 @@
+package sistemterdistribusi.tubessister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TubesSisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TubesSisterApplication.class, args);
+	}
+
+}
---
+++ tubes-sister/src/test/java/sistemterdistribusi/tubessister/TubesSisterApplicationTests.java
@@ -0,0 +1,13 @@
+package sistemterdistribusi.tubessister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TubesSisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
